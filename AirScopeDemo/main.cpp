e1b0bf05f9f5edc98d0569d4ad3224b3be0a072e
Saumitro Dasgupta <ethereon@xinverse.com>
2014-08-23 21:26:39 -0700

    Implemented Lorenz attractor demo.

--- AirScopeDemo/main.cpp
+++ AirScopeDemo/main.cpp
@@ -8,15 +8,41 @@
 
 #include <iostream>
 #include <cmath>
+#include <unistd.h>
 #include <ASTransmit/Plotter.hpp>
 
-int main(int argc, const char * argv[])
+void plot_helix()
 {
     as::Plotter plt("Helix");
     for(float theta=0; theta<10*M_PI; theta+=0.01)
     {
         plt.plot(theta, sin(theta), cos(theta));
     }
+}
+
+void plot_lorenz_attractor(float sigma = 10.0f,
+                           float rho = 28.0f,
+                           float beta = 8.0f/3.0f,
+                           int numIters = 1000,
+                           float delay = 2000)
+{
+    as::Plotter plt("Lorenz Attractor");
+    const float dt=0.01f;
+    float x=0.1f, y=0.0f, z=0.0f;
+    for(int i=0; i<numIters; ++i)
+    {
+        float nx = x + dt*sigma*(y-x);
+        float ny = y + dt*(x*(rho-z) - y);
+        float nz = z + dt*(x*y - beta*z);
+        x = nx; y = ny; z = nz;
+        plt.plot(x, y, z);
+        usleep(delay);
+    }
+}
+
+int main(int argc, const char * argv[])
+{
+    plot_lorenz_attractor();
     return 0;
 }
 
