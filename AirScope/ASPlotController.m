26a32dd06935aed3489101350b8ff71097408d98
Saumitro Dasgupta <ethereon@xinverse.com>
2014-08-24 22:55:38 -0700

    Automatic + keyboard-based plot rotation.

--- AirScope/ASPlotController.m
+++ AirScope/ASPlotController.m
@@ -10,11 +10,13 @@
 #import "ASLine3D.h"
 #import "ASPlane.h"
 #import "ASPointCloud.h"
+#import <Carbon/Carbon.h>
 
 static NSString* const kGroundPlaneKey = @"__as-ground-plane";
 
 @interface ASPlotController ()
 @property (strong) NSMutableDictionary* elements;
+@property (strong) NSTimer* rotationTimer;
 @end
 
 @implementation ASPlotController
@@ -26,6 +28,11 @@ static NSString* const kGroundPlaneKey = @"__as-ground-plane";
     _plot = [[ASPlot alloc] init];
     [self addElement:[[ASPlane alloc] init] withKey:kGroundPlaneKey];
     return self;
+}
+
+-(void) dealloc
+{
+    [self stopAutoRotation];
 }
 
 - (void)windowDidLoad
@@ -75,6 +82,85 @@ static NSString* const kGroundPlaneKey = @"__as-ground-plane";
 -(void) addPoint:(GLKVector3)p toCloudWithKey:(NSString*)cloudKey
 {
     [self addPoint:p toElementOfClass:[ASPointCloud class] withKey:cloudKey];
+}
+
+-(void) keyDown:(NSEvent *)theEvent
+{
+    int keyCode = [theEvent keyCode];
+    switch (keyCode)
+    {
+        case kVK_Return:
+            [self toggleAutoRotate];
+            break;
+        case kVK_LeftArrow:
+            [self incrementRotationX:0 y:[self manualRotationIncrement]];
+            break;
+        case kVK_RightArrow:
+            [self incrementRotationX:0 y:-[self manualRotationIncrement]];
+            break;
+        case kVK_UpArrow:
+            [self incrementRotationX:[self manualRotationIncrement] y:0];
+            break;
+        case kVK_DownArrow:
+            [self incrementRotationX:-[self manualRotationIncrement] y:0];
+            break;
+        case kVK_ANSI_R:
+            [self resetTransform];
+            break;
+    }
+}
+
+#pragma mark - Plot Transformations
+
+-(void) toggleAutoRotate
+{
+    if(_rotationTimer!=nil)
+    {
+        [_rotationTimer invalidate];
+        _rotationTimer = nil;
+    }
+    else
+    {
+        _rotationTimer = [NSTimer scheduledTimerWithTimeInterval:0.05
+                                                          target:self
+                                                        selector:@selector(handleRotationTimer:)
+                                                        userInfo:nil
+                                                         repeats:YES];
+    }
+}
+
+-(float) manualRotationIncrement
+{
+    return 0.1;
+}
+
+-(void) incrementRotationX:(float)deltaX y:(float)deltaY
+{
+    [_plot setXRotation:[_plot xRotation]+deltaX];
+    [_plot setYRotation:[_plot yRotation]+deltaY];
+    [_plotView setNeedsDisplay:YES];
+}
+
+-(void) handleRotationTimer:(NSTimer*)timer
+{
+    [self incrementRotationX:0.0 y:0.02];
+}
+
+-(void) stopAutoRotation
+{
+    if(_rotationTimer!=nil)
+    {
+        [_rotationTimer invalidate];
+        _rotationTimer = nil;
+    }
+}
+
+-(void) resetTransform
+{
+    [self stopAutoRotation];
+    [_plot setXRotation:0];
+    [_plot setYRotation:0];
+    [_plotView setNeedsDisplay:YES];
 }
 
 @end
